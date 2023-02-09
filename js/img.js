c1a8ec83ed11593ee69714811f35b13f8c633981
Willyan Patrykc <96625051+WILLY4N@users.noreply.github.com>
2023-02-09 10:26:14 -0300

    commit inicial
    1st commit

---
+++ js/img.js
@@ -0,0 +1,9 @@
+const imgs = [
+    '<img id="photo" src="imgs/morgan-freeman.jpg">', 
+    '<img id="photo" src="imgs/bruce-lee-you-tube.jpg">', 
+    '<img id="photo" src="imgs/mel-gibson.jpg">',
+    '<img id="photo" src="imgs/will-smith.webp">',
+    '<img id="photo" src="imgs/obama.jpg">'
+];
+
+var img = imgs[Math.floor(Math.random() * (imgs.length))];
